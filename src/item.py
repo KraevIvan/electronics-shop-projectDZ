62caf32c0be743a1caa93ec79e899286bb92a6cf
KraevIvan <KraevIvanA@yandex.ru>
2024-03-02 00:28:22 +0300

    add class Item

--- src/item.py
+++ src/item.py
@@ -13,7 +13,14 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append({"name": self.name, "price": self.price, "quantity": self.quantity})
+
+    def __repr__(self):
+        return f"name: {self.name}, price: {self.price}, quantity: {self.quantity}"
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +28,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
