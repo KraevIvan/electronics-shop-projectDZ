26bae7eac9a4a2d2fda52a0323c56726f298ca8b
KraevIvan <KraevIvanA@yandex.ru>
2024-03-13 16:04:14 +0300

    add tests for phone

---
+++ tests/test_phone.py
@@ -0,0 +1,18 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def phone1():
+    samsung = Phone("samsung", 20000, 4, 2)
+    return samsung
+
+
+def test_init(phone1):
+    assert phone1.price == 20000
+    assert phone1.name == "samsung"
+    assert phone1.quantity == 4
+    assert phone1.number_of_sim == 2
+    assert Phone("Meizu", 25000, 3, -1) == ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+    phone1.number_of_sim = 1
+    assert phone1.number_of_sim == 1
